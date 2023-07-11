76e6b026c1cc2728480de4b17ae154989b4d2372
Radnai Miklós <radnaimiki@gmail.com>
2023-07-11 13:38:57 +0200

    Add encode and decode to CodeAlg

--- codealg.cpp
+++ codealg.cpp
@@ -9,3 +9,32 @@ int charToInt(char c){
     assert(ascii >= (int)'a' && ascii <= (int)'z');
     return ascii - (int)'a';
 }
+char intToChar(int val){
+    if(val == 26)
+        return ' ';
+    assert(val >= 0 && val <= 25);
+    char c = (int)'a' + val;
+    return c;
+}
+string encode(const string& aMessage, const string& aKey){
+    string code = aMessage;
+    for(int i = 0 ; i < aMessage.size() ; ++i){
+        int valCode = charToInt(aKey.at(i));
+        int valMsg = charToInt(aMessage.at(i));
+        int codeVal = (valCode + valMsg) % 27;
+        // get the character from the character's code:
+        code.at(i) = intToChar(codeVal);
+    }
+    return code;
+}
+string decode(const string& aCode, const string& aKey){
+    string decoded = aCode;
+    for(int i = 0 ; i < aCode.size() ; ++i){
+        int valCode = charToInt(aKey.at(i));
+        int valKey = charToInt(aCode.at(i));
+        int codeVal = (valCode - valKey) % 27;
+        // get the character from the character's code:
+        decoded.at(i) = intToChar(codeVal);
+    }
+    return decoded;
+}
--- codealg.h
+++ codealg.h
@@ -1,2 +1,8 @@
 #pragma once
+
+#include <string>
+using std::string;
+
 int charToInt(char c);
+string encode(const string& aMessage, const string& aKey);
+string decode(const string& aCode, const string& aKey);
